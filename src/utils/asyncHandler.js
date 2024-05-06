d787f898514b52f8cd4d85a0673624ed5fbf8fde
archit <1980archit.sinhgh@gmail.com>
2024-05-06 20:11:59 +0530

    utils code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,1 +1,22 @@
-//const asyncHandler = ()  =>{}
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+/*const asyncHandler=(fnc)=>async(req,res,next)=>{
+
+    try {
+        
+    } catch (error) {
+        res.status(error.code||500).json({
+            success:false,,
+            message:error.message
+        })
+        
+    }
+}*/
